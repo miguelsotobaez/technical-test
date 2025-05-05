bd64e6ca543d26af283a0f9037a35ddc31547c84
Miguel Soto <miguelsotobaez@gmail.com>
2025-05-05 19:15:10 +0100

    reorganize main.ts for cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,25 +4,22 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const allowedOrigins = [
-    'https://ree-site.onrender.com',
-    'https://ree.misoba.dev',
-    'http://localhost',
-    'http://localhost:80',
-    'http://localhost:5173',
-  ];
-  
+  // Configuración de CORS
   app.enableCors({
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-    credentials: true,
+    origin: [
+      'https://ree-site.onrender.com',
+      'https://ree.misoba.dev',
+      'http://localhost',
+      'http://localhost:80',
+      'http://localhost:5173',
+    ],
     methods: ['GET', 'POST', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'Apollo-Require-Preflight'],
+    credentials: true,
+    allowedHeaders: [
+      'Content-Type',
+      'Authorization',
+      'Apollo-Require-Preflight',
+    ],
   });
 
   await app.listen(process.env.PORT ?? 3000);
